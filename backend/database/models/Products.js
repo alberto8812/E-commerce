c630983a9f90c8e1062e9b990c53a240a91d47e3
alberto88 <cvelascosaavedra@gmail.com>
2022-10-28 10:58:13 -0500

    models products and user were create

---
+++ backend/database/models/Products.js
@@ -0,0 +1,42 @@
+const {DataTypes}=require ('sequelize')
+
+module.exports=(sequelize)=>{
+
+'products',{
+    id:{
+        type:DataTypes.UUID,
+        defaultValue:DataTypes.UUIDV4,
+        primaryKey:true,
+        allownull:false
+    },
+    mame:{
+        type:DataTypes.STRING,
+        allownull:false,
+    },
+    price:{
+        type:DataTypes.FLOAT,
+        allownull:false,
+    },
+    stock:{
+        type:DataTypes.INTEGER,
+        allownull:false
+    },
+    detail:{
+        type:DataTypes.TEXT,
+        allownull:false
+    },
+    detail:{
+        type:DataTypes.TEXT,
+        allownull:false
+    },
+    image:{
+        type:DataTypes.TEXT,
+        allownull:false
+    },
+    delete:{
+        type:DataTypes.BOOLEAN,
+        default:false
+    }
+
+}
+}
---
+++ backend/database/models/Usuer.js
@@ -0,0 +1,53 @@
+
+const {DataTypes} = require ('sequelize') 
+module.exports = (sequelize) => {
+
+  sequelize.define('user', {
+
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      unique: true, 
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      unique: true, 
+      allowNull: false
+    }, 
+    
+    userName:{
+      type: DataTypes.STRING,
+      allowNull: false,
+       
+    },
+    direction:{
+      type: DataTypes.STRING,
+     
+       
+    },
+   city:{
+      type: DataTypes.STRING,
+     
+       
+    },
+    enabled:{
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue:true            
+    },
+    
+
+    flagAdmin:{
+      type: DataTypes.BOOLEAN,
+      allowNull: true,                
+    }
+
+  })
+}
