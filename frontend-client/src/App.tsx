3dcf9451fcfced3ab781164f3aab0a1e8b3e1a1f
alberto88 <cvelascosaavedra@gmail.com>
2022-11-03 21:37:12 -0500

    navbar create

--- frontend-client/src/App.tsx
+++ frontend-client/src/App.tsx
@@ -3,11 +3,13 @@ import './App.css';
 import Home from './components/pages/Home';
 import { Routes,Route } from "react-router-dom";
 import Details from './components/pages/Details';
+import Navbar from './components/component/navbar/Navbar';
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/detail/:id' element={<Details/>}/>
