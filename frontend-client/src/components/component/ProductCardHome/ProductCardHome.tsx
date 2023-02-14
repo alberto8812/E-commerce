a82f61c2509c4dd914f57c57ed99d966b8531f75
alberto88 <cvelascosaavedra@gmail.com>
2023-02-13 21:00:01 -0500

    create componente ProductCardHome

---
+++ frontend-client/src/components/component/ProductCardHome/ProductCardHome.tsx
@@ -0,0 +1,10 @@
+import { createContext, ReactElement } from 'react';
+
+export const ProductContext=createContext({})
+const {Provider}=ProductContext;
+
+export const ProductCardHome = () => {
+  return (
+    <div>ProductCardHome</div>
+  )
+}
