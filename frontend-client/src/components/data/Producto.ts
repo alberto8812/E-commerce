731ece5114a0118d8d81cff77f68120bf7b4e945
alberto88 <cvelascosaavedra@gmail.com>
2023-02-26 21:23:48 -0500

    css body grid

--- frontend-client/src/components/data/Producto.ts
+++ frontend-client/src/components/data/Producto.ts
@@ -6,6 +6,7 @@ const Audio={
                 img:[
                     'https://cdn.alkst.co/td92da290/img/474xt_hvcdgx_6e565404.jpg',
                     'https://cdn.alkst.co/td92da290/img/474xt_vh7bdy_f26220ed.jpg',
+                    'https://cdn.alkst.co/td92da290/img/474xt_vh7bdy_f26220ed.jpg',
                     
                     'https://cdn.alkst.co/td92da290/img/474xt_9r3ey4_b664f20a.jpg'
                 ]
